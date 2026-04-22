19b5315436a3fd3a9a4933258c1a533c7ad60644
wellingtonlucio82-dev <wellingtonlucio82@gmail.com>
2026-04-22 08:54:38 -0300

    atualizando commit

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("ligth")) {
     //se tiver ligth mode, addicionar a imagem ligth
-    img.setAttribute("src", "/assets/avatar-ligth.png");
+    img.setAttribute("src", "./assets/avatar-ligth.png");
   } else {
     //se não tiver ligth mode,adicionar a imagem dark
-    img.setAttribute("src", "/assets/avatar.png");
+    img.setAttribute("src", "./assets/avatar.png");
   }
 }
